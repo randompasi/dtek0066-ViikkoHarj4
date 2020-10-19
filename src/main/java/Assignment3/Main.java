e9c77152eeea9ffc571760a06c77f5fb721cbf63
randomapsi <anmivu@utu.fi>
2020-10-19 15:27:00 +0300

    Assignment3 finally right way

--- src/main/java/Assignment3/Main.java
+++ src/main/java/Assignment3/Main.java
@@ -4,10 +4,20 @@ package Assignment3;
 import java.util.*;
 
 public class Main {
-
+    /**
+     * most efficien could be custom hash code but that is problem,
+     * because it's most easiest to get error or collisions if nopt done right.
+     * Cannot say for default hascode and object.hash valueas are too close.
+     * @param args
+     */
 
     public static void main(String args[]){
-        List<HenkilöA> users = new ArrayList<>();
+
+        int y = 20;
+        int[] taulu = new int[y];
+
+        System.out.println();
+        Map<Integer,HenkilöA> users = new HashMap<>();
         Random r = new Random();
         for(int j = 0; j<500; j++) {
             int x = 10;
@@ -20,14 +30,17 @@ public class Main {
             }
             String foo = new String(array, java.nio.charset.Charset.forName("UTF-8"));
             HenkilöA h = new HenkilöA(foo, r.nextInt(100));
-            users.add(h);
+
+                taulu[h.hashCode()%taulu.length]+= 1;
+
         }
+        for(int i = 0; i < y; i++) System.out.print(taulu[i] + " ");
 
 
 
-            users.subList(0,19).forEach(x-> System.out.println(x.hashCode()));
-        System.out.println("--------------------------------------------------");
 
+        System.out.println("\n--------------------------------------------------");
+        int[]  taulu2 = new int[y];
         List<HenkilöB> users2 = new ArrayList<>();
         for(int j = 0; j<500; j++) {
             int x = 10;
@@ -40,15 +53,30 @@ public class Main {
             }
             String foo = new String(array, java.nio.charset.Charset.forName("UTF-8"));
             HenkilöB h = new HenkilöB(foo, r.nextInt(100));
-            users2.add(h);
+
+            taulu2[Math.abs(h.hashCode()%taulu2.length)]+= 1;
+
+        }
+        for(int i = 0; i < y; i++) System.out.print(taulu2[i] + " ");
+
+
+        System.out.println("\n--------------------------------------------------");
+        int[]  taulu3 = new int[y];
+        for(int j = 0; j<500; j++) {
+            int x = 10;
+            byte[] array = new byte[x];
+            new java.util.Random().nextBytes(array);
+            for (int i = 0; i < x; i++) {
+                array[i] &= 127;
+                array[i] %= 94;
+                array[i] += 32;
+            }
+            String foo = new String(array, java.nio.charset.Charset.forName("UTF-8"));
+            HenkilöA h = new HenkilöA(foo, r.nextInt(100));
+            taulu3[Objects.hash(h)%taulu3.length]+= 1;
         }
 
-
-        users2.subList(0,19).forEach(x-> System.out.println(x.hashCode()));
-
-        System.out.println("--------------------------------------------------");
-
-        System.out.println( Objects.hash(users));
+        for(int i = 0; i < y; i++) System.out.print(taulu3[i] + " ");
 
     }
 
